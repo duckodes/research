72ce964f0c978a6a819d26920cae3e2bcffc7f20
duckodes <bear90789078@gmail.com>
2025-03-20 15:08:38 +0800

    Add files via upload

--- module/post.js
+++ module/post.js
@@ -1,36 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 import fetcher from "./fetcher.js";
-const post = (() => {
-    async function autoUpdateData(database) {
-        const dataRef = ref(database, 'research/post');
-        const getFrontSection = (hash) => {
-            const index = hash.indexOf("?id=");
-            if (index !== -1) {
-                return hash.substring(0, index);
-            }
-            return hash;
-        }
-        onValue(dataRef, (snapshot) => {
-            let data = snapshot.val();
-            if (data !== null) {
-                let dataKeys = Object.keys(data);
-                let dataVals = Object.values(data);
-                for (let i = 0; i < dataKeys.length; i++) {
-                    const keys = dataKeys[i].split('?id=')[1];
-                    const name = getFrontSection(dataKeys[i]);
-                    const comment = dataVals[i];
-                }
-            }
-        });
-    }
-    return {
-        init: async () => {
-            const firebaseConfig = await fetcher.load('../config/firebaseConfig.json');
-            const app = initializeApp(firebaseConfig);
-            const database = getDatabase();
 
-            autoUpdateData(database);
+const firebaseConfig = await fetcher.load('../config/firebaseConfig.json');
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+const autoUpdateData = async () => {
+    const dataRef = ref(database, 'research/post');
+    const getFrontSection = (hash) => {
+        const index = hash.indexOf("?id=");
+        if (index !== -1) {
+            return hash.substring(0, index);
         }
+        return hash;
     }
-})();
+    onValue(dataRef, (snapshot) => {
+        let data = snapshot.val();
+        document.body.innerHTML = render(data);
+        console.log(snapshot);
+        if (data !== null) {
+            let dataKeys = Object.keys(data);
+            let dataVals = Object.values(data);
+            for (let i = 0; i < dataKeys.length; i++) {
+                const keys = dataKeys[i].split('?id=')[1];
+                const name = getFrontSection(dataKeys[i]);
+                const comment = dataVals[i];
+            }
+        }
+    });
+}
+await autoUpdateData();
+const render = (title) => {
+    return `
+        <div>This is ${title}</div>
+    `;
+}
