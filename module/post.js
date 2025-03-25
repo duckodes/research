4580bc5b3b913c5db570b221a062ed9a32840208
duckodes <bear90789078@gmail.com>
2025-03-25 12:20:34 +0800

    [!]修改編輯邏輯

--- module/post.js
+++ module/post.js
@@ -5,7 +5,7 @@ import fetcher from "./fetcher.js";
 
 const firebaseConfig = await fetcher.load('../config/firebaseConfig.json');
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); console.log(auth);
+const auth = getAuth(app);
 const database = getDatabase();
 document.body.innerHTML = '';
 
@@ -27,10 +27,16 @@ function logout() {
             console.log(error);
         });
 }
-const updatePost = (key, modifyKey, value) => {
-    const dataRef = ref(database, 'research/post/' + key + '/' + modifyKey);
+const updatePost = (key, title, content, img, detail, uid) => {
+    const dataRef = ref(database, 'research/post/' + key);
 
-    set(dataRef, value);
+    set(dataRef, {
+        title: title,
+        content: content,
+        img: img,
+        detail: detail,
+        permissions: uid
+    });
 }
 const post = (uid = null, title, content, img, detail) => {
     const dataHash = dateutils.ToHash();
@@ -44,67 +50,83 @@ const post = (uid = null, title, content, img, detail) => {
         permissions: uid
     });
 }
-// post('插件繪圖工具', 
+// post('DPcmhV427VQNJ9ojiOTD2aYyuE83',
+// '插件繪圖工具', 
 // 'unity底層開發的插件工具',
-// 'https://cdn.discordapp.com/attachments/1298197587584880674/1353299304386793482/color_all.png?ex=67e125b1&is=67dfd431&hm=707d2a3aa0821e7ac373a47880af23da1465550b94a23e9cb14d68de7c692478&',
+// 'https://cdn.discordapp.com/attachments/1298197587584880674/1353299304386793482/color_all.png?ex=67e31ff1&is=67e1ce71&hm=ab9b157472f48981b323a99a2af1c3c7d86060fe60232275393131afcdf211cc&',
 // 'ColorPanel插件開發<br>目的: Unity編輯器畫圖。<br>動機: 想畫個小UI物件，打開Photoshop卻很卡，因此就想直接用Unity引擎做出可以畫圖的小工具。');
 const autoUpdateData = async () => {
-    document.body.innerHTML += render.cardContainer.html();
+    if (!render.cardContainer.dom()) {
+        document.body.innerHTML += render.cardContainer.html();
+    }
     const dataRef = ref(database, 'research/post');
     onValue(dataRef, (snapshot) => {
-        document.querySelector('.card-container').innerHTML = '';
+        render.cardContainer.dom().innerHTML = '';
         let data = snapshot.val();
-        if (data !== null) {
-            let dataKeys = Object.keys(data);
-            let dataVals = Object.values(data);
-            const cardContainer = document.querySelector('.card-container');
-            for (let i = 0; i < dataKeys.length; i++) {
-                const keys = dataKeys[i];
-                const permissions = dataVals[i].permissions;
-                const title = dataVals[i].title;
-                const content = dataVals[i].content;
-                const img = dataVals[i].img;
-                const detail = dataVals[i].detail;
-                cardContainer.innerHTML += render.card.html(keys, title, img, content);
-                setTimeout(() => {
-                    render.card.dom(keys).card.addEventListener('click', () => {
-                        if (render.card.dom(keys).card.getAttribute('contenteditable') === 'true') return;
-                        if (render.card.dom(keys).detail.innerHTML === '') {
-                            render.card.dom(keys).detail.innerHTML = detail;
-                        } else {
-                            render.card.dom(keys).detail.innerHTML = '';
-                        }
-                    });
-                    if (auth.currentUser !== null && permissions === auth.currentUser.uid) {
-                        render.card.dom(keys).card.innerHTML += render.updatePost.html(keys);
-                        render.updatePost.dom(keys).modify.addEventListener('click', () => {
-                            if (render.card.dom(keys).card.getAttribute('contenteditable') !== 'true') {
-                                render.card.dom(keys).card.setAttribute('contenteditable', true);
-                                render.card.dom(keys).detail.innerHTML = detail;
-                                if (render.card.dom(keys).img) {
-                                    render.card.dom(keys).img.setAttribute('style', 'display: none;');
-                                    render.card.dom(keys).imgSrc.innerHTML = `${render.card.dom(keys).img.getAttribute('src')}`;
-                                }
-                                render.updatePost.dom(keys).confirm.setAttribute('style', '');
-                                render.updatePost.dom(keys).modify.setAttribute('style', 'display: none;');
-                            } else {
-                                render.card.dom(keys).card.setAttribute('contenteditable', false);
-                                render.card.dom(keys).img.setAttribute('style', '');
-                                render.card.dom(keys).imgSrc.innerHTML = '';
-                                render.updatePost.dom(keys).confirm.setAttribute('style', 'display: none;');
-                                render.updatePost.dom(keys).modify.setAttribute('style', 'display: none;');
-                            }
-                        });
-                        render.updatePost.dom(keys).confirm.addEventListener('click', () => {
-                            updatePost(keys, 'title', render.card.dom(keys).title.textContent);
-                            updatePost(keys, 'content', render.card.dom(keys).content.textContent);
-                            updatePost(keys, 'detail', render.card.dom(keys).detail.textContent);
-                        });
+        if (data === null) return render.cardContainer.dom().innerHTML = render.emptyContent.html();
+        let dataKeys = Object.keys(data);
+        let dataVals = Object.values(data);
+        const cardContainer = render.cardContainer.dom();
+        for (let i = 0; i < dataKeys.length; i++) {
+            const keys = dataKeys[i];
+            const permissions = dataVals[i].permissions;
+            const title = dataVals[i].title;
+            const content = dataVals[i].content;
+            const img = dataVals[i].img;
+            const detail = dataVals[i].detail;
+            cardContainer.innerHTML += render.card.html(keys, title, img, content);
+            setTimeout(() => {
+                render.card.dom(keys).card.addEventListener('click', () => {
+                    if (render.card.dom(keys).card.getAttribute('contenteditable') === 'true') return;
+                    if (render.card.dom(keys).detail.innerHTML === '') {
+                        render.card.dom(keys).detail.innerHTML = detail;
+                    } else {
+                        render.card.dom(keys).detail.innerHTML = '';
                     }
                 });
-            }
-        } else {
-            document.querySelector('.card-container').innerHTML = render.emptyContent.html();
+                // 底層開發
+                if (auth.currentUser !== null && permissions === auth.currentUser.uid) {
+                    render.card.dom(keys).card.innerHTML += render.updatePost.html(keys);
+                    render.updatePost.dom(keys).addEventListener('click', () => {
+                        if (render.card.dom(keys).card.getAttribute('contenteditable') !== 'true') {
+                            render.updatePost.dom(keys).textContent = '確認';
+                            render.card.dom(keys).card.setAttribute('contenteditable', true);
+
+                            // 圖片連結可視化編輯
+                            render.card.dom(keys).img.style.display = 'none';
+                            render.card.dom(keys).imgSrc.innerHTML = `${render.card.dom(keys).img.getAttribute('src')}`;
+                        } else {
+                            render.updatePost.dom(keys).textContent = '修改';
+                            render.card.dom(keys).card.setAttribute('contenteditable', false);
+
+                            // 更新
+                            updatePost(keys, render.card.dom(keys).title.innerHTML, render.card.dom(keys).content.innerHTML, render.card.dom(keys).imgSrc.innerHTML, render.card.dom(keys).detail.innerHTML, auth.currentUser.uid);
+
+                            // 圖片連結可視化編輯
+                            render.card.dom(keys).img.style.display = '';
+                            render.card.dom(keys).imgSrc.innerHTML = '';
+                        }
+                        // 內文
+                        render.card.dom(keys).detail.innerHTML = detail;
+                    });
+                    // 換行更換
+                    render.card.dom(keys).card.addEventListener('keydown', (event) => {
+                        if (event.key === 'Enter') {
+                            event.preventDefault();
+                            const selection = window.getSelection();
+                            if (selection.rangeCount > 0) {
+                                const range = selection.getRangeAt(0);
+                                const br = document.createElement('br');
+                                range.insertNode(br);
+                                range.setStartAfter(br);
+                                range.setEndAfter(br);
+                                selection.removeAllRanges();
+                                selection.addRange(range);
+                            }
+                        }
+                    });
+                }
+            });
         }
     });
 }
@@ -136,7 +158,7 @@ const render = {
             return `
                 <div class="card card-${id}">
                     <div class="title">${title}</div>
-                    ${src ? `<img src="${src}" alt="" draggable="false"/><div class="img-src"></div>` : ''}
+                    ${src ? `<img src="${src}" alt="" draggable="false"/><div class="img-src"></div>` : '<img src="Type URL" alt="" draggable="false"/><div class="img-src"></div>'}
                     <div class="content">${content}</div>
                     <div class="detail"></div>
                 </div>
@@ -169,16 +191,20 @@ const render = {
                 <div class="authentication">
                     <input type="text" class="account" size="10" placeholder="username.." />
                     <input type="password" class="password" size="10" placeholder="password.." />
-                    <div class="login">Login</div>
+                    <div class="container">
+                        <div class="login">Login</div>
+                        <div class="cancel">Cancel</div>
+                    </div>
                 </div>
             `;
         },
         dom: () => {
             return {
                 authentication: document.querySelector('.authentication'),
-                account: document.querySelector('.account'),
-                password: document.querySelector('.password'),
-                login: document.querySelector('.login')
+                account: document.querySelector('.authentication>.account'),
+                password: document.querySelector('.authentication>.password'),
+                login: document.querySelector('.authentication>.container>.login'),
+                cancel: document.querySelector('.authentication>.container>.cancel')
             }
         }
     },
@@ -205,15 +231,11 @@ const render = {
     updatePost: {
         html: (id) => {
             return `
-                <div class="modify-${id}" contenteditable="false">Modify</div>
-                <div class="confirm-${id}" contenteditable="false" style="display: none;">Confirm</div>
+                <div class="modify modify-${id}" contenteditable="false">修改</div>
             `;
         },
         dom: (id) => {
-            return {
-                modify: document.querySelector(`.modify-${id}`),
-                confirm: document.querySelector(`.confirm-${id}`)
-            }
+            return document.querySelector(`.modify-${id}`);
         }
     }
 }
@@ -222,7 +244,9 @@ const main = (async () => {
         console.log(user);
         const handleAddCardEvent = () => {
             if (!user) {
-                document.body.innerHTML += render.authentication.html();
+                if (!render.authentication.dom().authentication) {
+                    document.body.innerHTML += render.authentication.html();
+                }
                 render.authentication.dom().login.addEventListener('click', () => {
                     login(render.authentication.dom().account.value, render.authentication.dom().password.value, () => {
                         render.authentication.dom().authentication.remove();
@@ -239,6 +263,11 @@ const main = (async () => {
                         e.target.value = v.split('@')[0] + '@gmail.com';
                         render.authentication.dom().password.focus();
                     }
+                });
+                render.authentication.dom().cancel.addEventListener('click', async () => {
+                    await autoUpdateData();
+                    render.authentication.dom().authentication.remove();
+                    render.addCard.dom().addEventListener('click', handleAddCardEvent);
                 });
             } else {
                 document.body.innerHTML += render.admin.html().admin;
