866ba8709aaff4461acd6c0971dd92d207ddacd3
duckodes <bear90789078@gmail.com>
2025-03-26 17:04:15 +0800

    [!]修改一些樣式

--- module/post.js
+++ module/post.js
@@ -35,7 +35,8 @@ const updatePost = (key, title, content, img, detail, uid) => {
         content: content,
         img: img,
         detail: detail,
-        permissions: uid
+        permissions: uid,
+        time: dateutils.ToDateTime()
     });
 }
 const post = (uid = null, title, content, img, detail) => {
@@ -47,13 +48,14 @@ const post = (uid = null, title, content, img, detail) => {
         content: content,
         img: img,
         detail: detail,
-        permissions: uid
+        permissions: uid,
+        time: dateutils.ToDateTime()
     });
 }
 // post('DPcmhV427VQNJ9ojiOTD2aYyuE83',
 // '插件繪圖工具', 
-// 'unity底層開發的插件工具',
-// 'https://cdn.discordapp.com/attachments/1298197587584880674/1353299304386793482/color_all.png?ex=67e31ff1&is=67e1ce71&hm=ab9b157472f48981b323a99a2af1c3c7d86060fe60232275393131afcdf211cc&',
+// '<div style="color: #f06785;">unity底層開發的插件工具</div>',
+// 'https://lib.duckode.com/img/research/color_all.png',
 // 'ColorPanel插件開發<br>目的: Unity編輯器畫圖。<br>動機: 想畫個小UI物件，打開Photoshop卻很卡，因此就想直接用Unity引擎做出可以畫圖的小工具。');
 const autoUpdateData = async () => {
     document.body.insertAdjacentHTML('beforeend', render.cardContainer.html());
@@ -72,7 +74,9 @@ const autoUpdateData = async () => {
             const content = dataVals[i].content;
             const img = dataVals[i].img;
             const detail = dataVals[i].detail;
-            cardContainer.insertAdjacentHTML('beforeend', render.card.html(keys, title, img, content));
+            const time = dataVals[i].time;
+            // 所有人
+            cardContainer.insertAdjacentHTML('beforeend', render.card.html(keys, title, img, content, time));
             setTimeout(() => {
                 render.card.dom(keys).card.addEventListener('click', () => {
                     if (render.card.dom(keys).card.getAttribute('contenteditable') === 'true') return;
@@ -83,18 +87,18 @@ const autoUpdateData = async () => {
                     }
                 });
                 // 編輯
-                if (auth.currentUser !== null && permissions === auth.currentUser.uid && !render.updatePost.dom().domID(keys)) {
-                    render.card.dom(keys).card.insertAdjacentHTML('beforeend', render.updatePost.html(keys));
+                if (auth.currentUser !== null && permissions === auth.currentUser.uid) {
+                    render.card.dom(keys).card.insertAdjacentHTML('afterbegin', render.updatePost.html(keys));
                     render.updatePost.dom().domID(keys).addEventListener('click', () => {
                         if (render.card.dom(keys).card.getAttribute('contenteditable') !== 'true') {
-                            render.updatePost.dom().domID(keys).textContent = '確認';
+                            render.updatePost.dom().domID(keys).textContent = '✔';
                             render.card.dom(keys).card.setAttribute('contenteditable', true);
 
                             // 圖片連結可視化編輯
                             render.card.dom(keys).img.style.display = 'none';
                             render.card.dom(keys).imgSrc.innerHTML = `${render.card.dom(keys).img.getAttribute('src')}`;
                         } else {
-                            render.updatePost.dom().domID(keys).textContent = '修改';
+                            render.updatePost.dom().domID(keys).textContent = '✐';
                             render.card.dom(keys).card.setAttribute('contenteditable', false);
 
                             // 更新
@@ -163,6 +167,104 @@ const autoUpdateData = async () => {
                     });
                 }
             });
+            // 自己
+            render.addCard.dom().addEventListener('click', () => {
+                if (permissions === auth.currentUser.uid && !render.admin.dom().card(keys).card) {
+                    render.admin.dom().myCard.insertAdjacentHTML('beforeend', render.card.html(keys, title, img, content, time));
+                    setTimeout(() => {
+                        render.admin.dom().card(keys).card.addEventListener('click', () => {
+                            if (render.admin.dom().card(keys).card.getAttribute('contenteditable') === 'true') return;
+                            if (render.admin.dom().card(keys).detail.textContent === '') {
+                                render.admin.dom().card(keys).detail.innerHTML = detail;
+                            } else {
+                                render.admin.dom().card(keys).detail.innerHTML = '';
+                            }
+                        });
+                        // 編輯
+                        if (auth.currentUser !== null && !render.updatePost.dom().myDomID(keys)) {
+                            render.admin.dom().card(keys).card.insertAdjacentHTML('afterbegin', render.updatePost.html(keys));
+                            render.updatePost.dom().myDomID(keys).addEventListener('click', () => {
+                                if (render.admin.dom().card(keys).card.getAttribute('contenteditable') !== 'true') {
+                                    render.updatePost.dom().myDomID(keys).textContent = '✔';
+                                    render.admin.dom().card(keys).card.setAttribute('contenteditable', true);
+
+                                    // 圖片連結可視化編輯
+                                    console.log(render.admin.dom().card(keys).img);
+                                    render.admin.dom().card(keys).img.style.display = 'none';
+                                    render.admin.dom().card(keys).imgSrc.innerHTML = `${render.card.dom(keys).img.getAttribute('src')}`;
+                                } else {
+                                    render.updatePost.dom().myDomID(keys).textContent = '✐';
+                                    render.admin.dom().card(keys).card.setAttribute('contenteditable', false);
+
+                                    // 更新
+                                    updatePost(keys, render.admin.dom().card(keys).title.innerHTML, render.admin.dom().card(keys).content.innerHTML, render.admin.dom().card(keys).imgSrc.innerHTML, render.admin.dom().card(keys).detail.innerHTML, auth.currentUser.uid);
+
+                                    // 圖片連結可視化編輯
+                                    render.admin.dom().card(keys).img.style.display = '';
+                                    render.admin.dom().card(keys).img.setAttribute('src', render.admin.dom().card(keys).imgSrc.textContent);
+                                    render.admin.dom().card(keys).imgSrc.innerHTML = '';
+                                }
+                                // 內文
+                                render.admin.dom().card(keys).detail.innerHTML = detail;
+                            });
+                            // 換行更換
+                            render.admin.dom().card(keys).card.addEventListener('keydown', (event) => {
+                                if (event.key === 'Enter') {
+                                    event.preventDefault();
+                                    const selection = window.getSelection();
+                                    if (selection.rangeCount > 0) {
+                                        const range = selection.getRangeAt(0);
+                                        const br = document.createElement('br');
+                                        range.insertNode(br);
+                                        range.setStartAfter(br);
+                                        range.setEndAfter(br);
+                                        selection.removeAllRanges();
+                                        selection.addRange(range);
+                                    }
+                                }
+                                if (event.key === 'Backspace' || (event.ctrlKey && (event.key === 'x' || event.key === 'X'))) {
+                                    const selection = window.getSelection();
+                                    const activeElement = document.activeElement.querySelector('*'); // 獲取當前編輯的元素
+
+                                    if (activeElement) {
+                                        const isContentEmpty = activeElement.innerHTML.trim() === ''; // 判斷編輯內容是否為空
+
+                                        if (isContentEmpty) {
+                                            event.preventDefault(); // 阻止刪除操作
+                                            return; // 不再執行後續邏輯
+                                        }
+                                    }
+
+                                    if (selection.rangeCount > 0) {
+                                        const range = selection.getRangeAt(0);
+                                        const startContainer = range.startContainer;
+                                        const endContainer = range.endContainer;
+
+                                        // 檢查是否跨越多個節點
+                                        if (startContainer !== endContainer) {
+                                            event.preventDefault(); // 阻止刪除操作
+                                        } else {
+                                            // 如果選取範圍內包含 HTML 結構，才阻止刪除
+                                            const parentElement = startContainer.nodeType === Node.TEXT_NODE
+                                                ? startContainer.parentNode
+                                                : startContainer;
+
+                                            if (parentElement && parentElement.nodeType === Node.ELEMENT_NODE) {
+                                                const rangeContents = range.cloneContents();
+                                                const hasHtmlElements = rangeContents.querySelector('*') !== null; // 檢查範圍內是否包含 HTML 元素
+
+                                                if (hasHtmlElements) {
+                                                    event.preventDefault(); // 阻止刪除 HTML 結構
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     });
 }
@@ -190,13 +292,14 @@ const render = {
         }
     },
     card: {
-        html: (id, title, src, content) => {
+        html: (id, title, src, content, time) => {
             return `
                 <div class="card card-${id}">
                     <div class="title">${title}</div>
                     ${src ? `<img src="${src}" alt="" draggable="false"/><div class="img-src"></div>` : '<img src="Type URL" alt="" draggable="false"/><div class="img-src"></div>'}
                     <div class="content">${content}</div>
                     <div class="detail"></div>
+                    <div class="time" contenteditable="false">${time}</div>
                 </div>
             `;
         },
@@ -214,7 +317,7 @@ const render = {
     addCard: {
         html: () => {
             return `
-                <div class="add-card">+</div>
+                <div class="add-card">⤫</div>
             `;
         },
         dom: () => {
@@ -250,7 +353,13 @@ const render = {
                 admin: `
                     <div class="admin">
                         <div class="logout">Logout</div>
-                        <div class="home">\<</div>
+                        <div class="home">⤫</div>
+                        <input type="text" class="title" placeholder="title.."/>
+                        <input type="text" class="img-url" placeholder="image url.."/>
+                        <input type="text" class="content" placeholder="content.."/>
+                        <input type="text" class="detail" placeholder="detail.."/>
+                        <div class="upload">上傳</div>
+                        <div class="my-card"></div>
                     </div>
                 `,
                 addPost: `
@@ -262,22 +371,41 @@ const render = {
             return {
                 admin: document.querySelector('.admin'),
                 logout: document.querySelector('.admin>.logout'),
-                home: document.querySelector('.admin>.home')
+                home: document.querySelector('.admin>.home'),
+                title: document.querySelector('.admin>.title'),
+                imgUrl: document.querySelector('.admin>.img-url'),
+                content: document.querySelector('.admin>.content'),
+                detail: document.querySelector('.admin>.detail'),
+                upload: document.querySelector('.admin>.upload'),
+                myCard: document.querySelector('.admin>.my-card'),
+                card: (id) => {
+                    return {
+                        card: document.querySelector(`.admin>.my-card>.card-${id}`),
+                        detail: document.querySelector(`.admin>.my-card>.card-${id}>.detail`),
+                        content: document.querySelector(`.admin>.my-card>.card-${id}>.content`),
+                        title: document.querySelector(`.admin>.my-card>.card-${id}>.title`),
+                        img: document.querySelector(`.admin>.my-card>.card-${id}>img`),
+                        imgSrc: document.querySelector(`.admin>.my-card>.card-${id}>.img-src`),
+                    }
+                }
             }
         }
     },
     updatePost: {
         html: (id) => {
             return `
-                <div class="modify modify-${id}" contenteditable="false">修改</div>
+                <div class="modify modify-${id}" contenteditable="false">✐</div>
             `;
         },
         dom: () => {
             return {
                 domID: (id) => {
-                    return document.querySelector(`.modify-${id}`);
+                    return document.querySelector(`.card-${id}>.modify-${id}`);
                 },
-                dom: document.querySelectorAll('.modify')
+                dom: document.querySelectorAll('.modify'),
+                myDomID: (id) => {
+                    return document.querySelector(`.my-card>.card>.modify-${id}`);
+                }
             }
         }
     }
@@ -303,6 +431,20 @@ const main = (async () => {
                     login(render.authentication.dom().account.value, render.authentication.dom().password.value, async () => {
                         location.reload();
                     });
+                });
+                render.authentication.dom().account.addEventListener('keydown', e => {
+                    if (e.key === 'Enter') {
+                        login(render.authentication.dom().account.value, render.authentication.dom().password.value, async () => {
+                            location.reload();
+                        });
+                    }
+                });
+                render.authentication.dom().password.addEventListener('keydown', e => {
+                    if (e.key === 'Enter') {
+                        login(render.authentication.dom().account.value, render.authentication.dom().password.value, async () => {
+                            location.reload();
+                        });
+                    }
                 });
                 render.authentication.dom().account.addEventListener('input', (e) => {
                     const v = e.target.value;
@@ -330,6 +472,9 @@ const main = (async () => {
                 });
                 render.admin.dom().home.addEventListener('click', () => {
                     render.admin.dom().admin.remove();
+                });
+                render.admin.dom().upload.addEventListener('click', () => {
+                    post(user.uid, render.admin.dom().title.value, render.admin.dom().content.value, render.admin.dom().imgUrl.value, render.admin.dom().detail.value);
                 });
             });
         }
